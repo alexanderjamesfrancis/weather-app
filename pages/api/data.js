c9309ea99618b59993096de8842f97aafcfd31ae
Alex Francis <alexanderjamesfrancis@hotmail.co.uk>
2022-10-07 11:55:28 +0100

    deploy fixes

--- pages/api/data.js
+++ pages/api/data.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
     const getWeatherData = await fetch(
         //Sort API key
         `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${process.env.OPENWEATHERMAP_API_KEY}`
-        //`https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric$appid=${process.env.OPENWEATHERMAP_API_KEY}`
+        
     )
     console.log(getWeatherData);
 
